ff3de94ef23aac6152f113636d77601cda2a5931
Liz-cloud <mfnlin003@myuct.ac.za>
2021-06-15 14:38:28 +0200

    test torch library functions

--- Classifier.py
+++ Classifier.py
@@ -11,7 +11,12 @@ import matplotlib.pyplot as plt
 import torchvision
 
 if __name__ == '__main__':
+
     print("torch with zeros:\n", torch.zeros(5,5))
     print("torch that is empty:\n ", torch.empty(5,5))
     print("torch with random numbers:\n", torch.rand(5,5))
+    a=torch.rand(5,5)
+    b=torch.rand(5,5)
+    print("combine a and b using '+':\n",a+b)
+    print("combine a and b using 'torch.add()' method:\n",torch.add(a,b))
     # device = torch.device("cuda" if torch.a.is_avalable() else "cpu")
