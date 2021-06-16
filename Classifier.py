67ff78da99fd4f22c159412f80c69a7437213040
Liz-cloud <mfnlin003@myuct.ac.za>
2021-06-16 03:31:07 +0200

    shallow leaning

--- Classifier.py
+++ Classifier.py
@@ -13,39 +13,105 @@ import matplotlib.pyplot as plt
 # transforms to transform images
 from torchvision import transforms
 from torch import optim
-import os
-import sys
-script_location = os.path.split(os.path.realpath(sys.argv[0]))[0]
+from time import time
+# import os
+# import sys
 
-if __name__ == '__main__':
+# script_location = os.path.split(os.path.realpath(sys.argv[0]))[0]
 
-    # used to put all data into gpu and model include gpu and  we can train all of these together inside the gpu itself
 
+def main():
+    # used to put all data into gpu and model include gpu
+    # and  we can train all of these together inside the gpu itself
+    # when you have huge amount of data use a gpu system so you can train your data faster
+    # you can get the reserves faster to analyze stuff
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device)
 
     # hyper - parameters declared and set
-    num_epochs=10
-    num_classes=10
-    gamma=0.001
-    batch_size=64
-    input_size=784 # 28 x 28
-    hidden_layer=100 # hidden neurons
+    # change num of epochs , gamma, batch size to give better output
+    num_epochs = 10
+    num_classes = 10
+    gamma = 0.001
+    batch_size = 64
+    input_size = 784  # 28 x 28
+    hidden_layer = 100  # hidden neurons
     # script_location = os.path.split(os.path.realpath(sys.argv[0]))[0]
     # print(script_location)
 
-    train_data = torchvision.datasets.MNIST(root="./",train=True,transform=transforms.ToTensor(), download=False)
-    test_data=torchvision.datasets.MNIST(root="./",train=False,transform=transforms.ToTensor(),download=False)
+    train_data = torchvision.datasets.MNIST(root="./", train=True, transform=transforms.ToTensor(), download=False)
+    test_data = torchvision.datasets.MNIST(root="./", train=False, transform=transforms.ToTensor(), download=False)
 
-    train_loader=torch.utils.data.DataLoader(dataset=train_data, batch_size=batch_size,shuffle=True, num_workers=2)
-    test_loader=torch.utils.data.DataLoader(dataset=test_data, batch_size=batch_size,shuffle=False, num_workers=2)
+    train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True, num_workers=2)
+    test_loader = torch.utils.data.DataLoader(dataset=test_data, batch_size=batch_size, shuffle=False, num_workers=2)
 
     # check the data samples
-    check_data= iter(train_loader)
-    image,label=next(check_data)
-    print(image.shape,label.shape)
+    check_data = iter(train_loader)
+    image, label = next(check_data)
+    print(image.shape, label.shape)
 
     # try to manipulate and view the images
-    for i in range(1,60+1):
-        plt.subplot(6,10,i)
-        plt.imshow(image[0].numpy().squeeze(),cmap='gray_r')
+    for i in range(1, 60 + 1):
+        print(label[i])
+        plt.subplot(6, 10, i)
+        plt.imshow(image[0].numpy().squeeze(), cmap='gray_r')
+    plt.show()
+
+    # implement model for Neural Network
+    # increase the number of  Layers for deep learning/network
+    # use more hidden layers to reduce the loss
+    # using one hidden layer makes a shallow neural network
+    model = nn.Sequential(
+        nn.Linear(in_features=input_size, out_features=hidden_layer),
+        nn.ReLU(),
+        nn.Linear(in_features=hidden_layer, out_features=hidden_layer),
+        nn.ReLU(),
+        nn.Linear(in_features=hidden_layer, out_features=hidden_layer),
+        nn.ReLU(),
+        nn.Linear(in_features=hidden_layer, out_features=hidden_layer),
+        nn.ReLU(),
+        nn.Linear(in_features=hidden_layer, out_features=num_classes)
+    )
+    repr(model)
+    # the loss using the cross Entropy loss because when it is multi class classification
+    # but if it was binary class we did use Binary CLass entropy loss
+    criterion = nn.CrossEntropyLoss()
+    # Optimizers require the parameters to optimize and a learning rate
+    optimizer = optim.Adam(model.parameters(), lr=gamma)
+
+    samples = len(train_loader)  # len total samples
+
+    # put model inside of gpu
+    #
+    model = model.to(device)
+
+    # make training loop COre of Neural Network
+    time0=time()
+    for ep in range(num_epochs):
+        for step, (image, label) in enumerate(train_loader):
+            # pass images to device
+            image = image.reshape(-1, 784).to(device)
+            # pass labels to device
+            label = label.to(device)
+
+            # get output
+            output = model(image)
+            # get loss
+            loss = criterion(output, label)
+
+            # Clear the gradients, do this because gradients are accumulated, Training pass:
+            optimizer.zero_grad()
+            # go back using back propagation by zeroing the gradients, then update weights
+            # This is where the model learns using backpropagation:
+            loss.backward()
+            # Take an update step and few the new weights i.e. Optimize the weights:
+            optimizer.step()
+
+            print("Epoch-> {}/{}, Step-> {}/{}, Loss-> {:.4f}".format(ep, num_epochs, step, samples, loss.item()))
+
+            # if training losses are decreases that is a good thing
+    print("\nTime taken to train is (in minutes)-> ",(time() -time0)/60)
+
+
+if __name__ == '__main__':
+    main()
